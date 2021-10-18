c53f421c1d0c8ef586a9aaa79a30c3f3c445be15
“Chej <chejahamed@gmail.com>
2021-10-18 15:50:05 +0100

    Add update function to TodoList

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
+// eslint-disable-next-line no-unused-vars
 import TodoForm from './TodoForm';
-import TodoList from './TodoList';
 
-function Todo() {
+function Todo({ todos, completeTodo, removeTodo }) {
+  // eslint-disable-next-line no-unused-vars
   const [edit, setEdit] = useState({
     id: null,
     value: ''
   });
-  return TodoList.map((todo, index) => (
+  return todos.map((todo, index) => (
     <>
       <div
         className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+        // eslint-disable-next-line react/no-array-index-key
         key={index}
       >
         <div key={todo.id} onClick={() => completeTodo(todo.id)}>{todo.text}</div>
 
       </div>
       <div className="icons">
-        d
+        <RiCloseCircleLine onClick={() => removeTodo(todo.id)} className="delete-icon" />
+        <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text })} className="edit-icon" />
       </div>
     </>
   ));
--- todo/src/components/TodoList.jsx
+++ todo/src/components/TodoList.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+import Todo from './Todo';
 import TodoForm from './TodoForm';
 
 function TodoList() {
@@ -13,10 +14,37 @@ function TodoList() {
     setTodos(newTodos);
     console.log(newTodos);
   };
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+    setTodos((prev) => prev.map((item) => (item.id === todoId ? newValue : item)));
+  };
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        // eslint-disable-next-line no-param-reassign
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   return (
     <div>
       <h1>Whats is the plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
 
   );
