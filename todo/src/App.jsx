852ff34bdac9700d08a9cab14208e0a984949559
“Chej <chejahamed@gmail.com>
2021-10-18 11:30:24 +0100

    Add todoList basic structure -Complete main TodoForm

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="todo-app">
-      <h1>Todo App</h1>
+      <TodoForm />
     </div>
   );
 }
--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -1,15 +1,28 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 
-function TodoForm() {
+function TodoForm(props) {
+  // eslint-disable-next-line no-unused-vars
   const [input, setInput] = useState('');
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDeault();
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+  };
   return (
-    <form className="todo-form">
+    <form className="todo-form" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Add a todo"
         value={input}
         name="text"
         className="todo-input"
+        onChange={handleChange}
       />
       <button type="button" className="todo-button">Add todo</button>
     </form>
