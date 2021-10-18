87e49cf3b0f1ec6ab1293c965a700056d5088438
“Chej <chejahamed@gmail.com>
2021-10-18 11:09:41 +0100

    Add todo-form basic structure

---
+++ todo/src/components/TodoForm.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function TodoForm() {
+  const [input, setInput] = useState('');
+  return (
+    <form className="todo-form">
+      <input
+        type="text"
+        placeholder="Add a todo"
+        value={input}
+        name="text"
+        className="todo-input"
+      />
+      <button type="button" className="todo-button">Add todo</button>
+    </form>
+
+  );
+}
+
+export default TodoForm;
